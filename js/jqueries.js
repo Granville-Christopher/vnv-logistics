898848fa615670b714cedc70252ee87ede7ec7be
Granville Christopher <christophergranville2@gmail.com>
2024-01-22 02:56:07 -0800

    first commit

---
+++ js/jqueries.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('#glb').click(function(){
+        $('#ext').slideToggle("slow")
+    })
+})
+
+$(document).ready(function(){
+    $('#rel').click(function(){
+        $('#con').slideToggle("slow")
+    })
+})
+
+$(document).ready(function(){
+    $('#cus').click(function(){
+        $('#per').slideToggle("slow")
+    })
+})
+
+$(document).ready(function(){
+    $('#tec').click(function(){
+        $('#mod').slideToggle("slow")
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
